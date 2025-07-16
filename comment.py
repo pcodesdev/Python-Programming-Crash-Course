82fa00178fa363121f3a1f5898680ad9b905a476
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2025-07-16 18:21:59 +0300

    include python installation process on the 3 major OSs

--- comment.py
+++
@@ -1,4 +0,0 @@
-#A comment is a note to a programmer.   
-#Comments are ignored by Python.
-#Comments are useful for documenting code.
-#The main purpose of comments is to explain code.
--- favourite_number.py
+++
@@ -1,3 +0,0 @@
-favourite_number = 13
-message = f"My favourite number is {favourite_number}."
-print(message)  # My favourite number is 13.
