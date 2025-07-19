06f89b58a420bc95bb07205a4e6125255bfef5dd
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2025-07-19 11:03:27 +0300

    Have completed learninga nd practicing on python numbers

---
+++ Part I Basics/variables_and_simple_data_types/numbers.py
@@ -0,0 +1,25 @@
+# Python calls any number with a decimal point a float. 
+# Integers and Floats
+# Underscores in Numbers
+# When you’re writing long numbers, you can group digits using underscores 
+# to make large numbers more readable:
+# >>> universe_age = 14_000_000_000
+# Multiple Assignment
+# You can assign values to more than one variable using just a single line of 
+# code. This can help shorten your programs and make them easier to read; 
+# you’ll use this technique most often when initializing a set of numbers.
+# For example, here’s how you can initialize the variables x, y, and z to zero:
+# >>> x, y, z = 0, 0, 0
+
+# Constants
+# A constant is a variable whose value stays the same throughout the life of a 
+# program.
+# Python programmers use all capital letters to indicate a viariable should be treated as a constant and never be changed
+# MAX_CONNECTIONS = 5000
+
+print(4 + 4)
+print(2 * 4)
+print(12 - 4)
+print(16 / 2)
+favorite_number = 10
+print(f"My favorite number is: {favorite_number}")
