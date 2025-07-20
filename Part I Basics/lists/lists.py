e81ac9138e6ab008352cc9cf94aaf77b9c7818b6
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2025-07-20 17:59:46 +0300

    Practised list manipulation techniques on adding and removing items from a list

--- Part I Basics/lists/lists.py
+++ Part I Basics/lists/lists.py
@@ -21,3 +21,74 @@ print(bicycles[-1])
 
 # Using Individual Values from a List
 print(f"My first bicycle was {bicycles[0].title()}.")
+
+# Modifying, Adding, and Removing Elements
+# Modifying Elements in a List
+# The syntax for modifying an element is similar to the syntax for accessing 
+# an element in a list. To change an element, use the name of the list followed 
+# by the index of the element you want to change, and then provide the new 
+# value you want that item to have.
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[1] = 'kuga'
+
+print(motorcycles)
+
+# Adding Elements to a List
+# ppending Elements to the End of a List
+# The simplest way to add a new element to a list is to append the item to the 
+# list. When you append an item to a list, the new element is added to the end 
+# of the list. Using the same list we had in the previous example, we’ll add the 
+# new element 'ducati' to the end of the list:
+
+motorcycles.append("bmw")
+print(motorcycles)
+
+# Inserting Elements into a List
+# You can add a new element at any position in your list by using the insert()
+# method. You do this by specifying the index of the new element and the 
+# value of the new item:
+motorcycles.insert(0, 'kingbird')
+print(motorcycles)
+
+# Removing Elements from a List
+# Removing an Item Using the del Statement
+# If you know the position of the item you want to remove from a list, you can 
+# use the del statement:
+del motorcycles[4]
+print(motorcycles)
+
+# Removing an Item Using the pop() Method
+# The pop() method removes the last item in a list, but it lets you work 
+# with that item after removing it. The term pop comes from thinking of a 
+# list as a stack of items and popping one item off the top of the stack. In this 
+# analogy, the top of a stack corresponds to the end of a list.
+
+removed_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(removed_motorcycle)
+print(f"The last motorcycle that I bought was a {removed_motorcycle} from Japan.")
+
+# Popping Items from Any Position in a List
+# You can use pop() to remove an item from any position in a list by including 
+# the index of the item you want to remove in parentheses:
+
+
+
+first_motorcycle = motorcycles.pop(0)
+
+print(f"The first motorcycle that I owned was a {first_motorcycle.title()} from China.")
+
+# when you want to delete an item from a list 
+# and not use that item in any way, use the del statement; if you want to use an 
+# item as you remove it, use the pop() method.
+# Removing an Item by Value
+# Sometimes you won’t know the position of the value you want to remove 
+# from a list. If you only know the value of the item you want to remove, you 
+# can use the remove() method.
+expensive_motorcycle = 'kuga'
+motorcycles.remove(expensive_motorcycle)
+print(motorcycles)
+print(f"{expensive_motorcycle.title()} is a very expensive motorcycle with limited spareparts availability in the market.")
--- Part I Basics/lists/try_it_myself.py
+++ Part I Basics/lists/try_it_myself.py
@@ -11,3 +11,57 @@ print(message)
 my_mode_of_trabsportation = ['Motorbike', 'Train', "Aeroplane"]
 
 print(f"My favorite mode of transport is by {my_mode_of_trabsportation[2]}.")
+
+dinner_guest_list = ['Jememiah', 'Samuel', 'Lavenda']
+guest_one_message = f"Hi {dinner_guest_list[0]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_two_message = f"Hi {dinner_guest_list[1]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_three_message = f"Hi {dinner_guest_list[2]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+
+print(guest_one_message)
+print(guest_two_message)
+print(guest_three_message)
+cannot_attend = dinner_guest_list.pop(0)
+print(f"Unfortunately {cannot_attend} will not be attending the party.")
+print(dinner_guest_list)
+dinner_guest_list.insert(0, 'Emmah')
+print(dinner_guest_list)
+
+guest_one_message = f"Hi {dinner_guest_list[0]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+print(f'{guest_one_message}\n{guest_two_message}\n{guest_three_message}\n')
+
+print("I have some good news. I found a bigger table and this more guest!")
+dinner_guest_list.insert(0, 'Stephen')
+dinner_guest_list.insert(2, 'Esther')
+dinner_guest_list.append("Ann")
+print(dinner_guest_list)
+
+guest_one_message = f"Hi {dinner_guest_list[0]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_two_message = f"Hi {dinner_guest_list[1]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_three_message = f"Hi {dinner_guest_list[2]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_four_message = f"Hi {dinner_guest_list[3]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_five_message = f"Hi {dinner_guest_list[4]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_six_message = f"Hi {dinner_guest_list[5]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+
+# 
+
+print(f'{guest_one_message}\n{guest_two_message}\n{guest_three_message}\n{guest_four_message}\n{guest_five_message}\n{guest_six_message}\n')
+
+print("Friends sorry for communicating this rate, unfortunately the table won't be arriving on time and I will have a table that can only accomodate 2 people.")
+removed_guest = dinner_guest_list.pop()
+print(f"{removed_guest} I can't manage to invite you for the dinner due to space constraints.")
+removed_guest = dinner_guest_list.pop()
+print(f"{removed_guest} I can't manage to invite you for the dinner due to space constraints.")
+removed_guest = dinner_guest_list.pop()
+print(f"{removed_guest} I can't manage to invite you for the dinner due to space constraints.")
+removed_guest = dinner_guest_list.pop()
+print(f"{removed_guest} I can't manage to invite you for the dinner due to space constraints.")
+print(dinner_guest_list)
+
+guest_one_message = f"Hi {dinner_guest_list[0]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+guest_two_message = f"Hi {dinner_guest_list[1]}, I hope you are doing well I am officially inviting you to a dinner party at Panari hotel today at 7:30 PM"
+
+print(f'{guest_one_message}\n{guest_two_message}\n')
+
+del dinner_guest_list[0]
+del dinner_guest_list[0]
+print(dinner_guest_list)
