f3de5c328c447226ad4789724018b8dad1cb86e9
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2025-08-20 17:56:12 +0300

    Have started working on python conditional statements

---
+++ Part I Basics/Statements/cars.py
@@ -0,0 +1,13 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+        
+# Checking for Inequality
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print('Hold the anchovies!')
+
+# Checking Multiple Conditions or & and
